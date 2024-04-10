0358c485b0d0a2d98c1e982d2da6b82c4de5a992
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-10 05:29:12 -0500

    Add email validation.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import "./style.css";
+import './style.css';
 
 const emailInput = document.getElementById('email');
-const feedbackElement = document.getElementById('emailFeedback');
 
-emailInput.addEventListener('blur', function() {
-    if (this.validity.valid) {
-        // If the email input matches the pattern and is not empty
-        feedbackElement.style.display = 'none';
-    } else {
-        // If the input is invalid
-        feedbackElement.style.display = 'block';
+function emailFeedback(input, feedbackElement) {
+  if (input.validity.valid) {
+    feedbackElement.style.display = 'none';
+  } else {
+    // Check different types of validity errors and set messages accordingly
+    if (input.validity.valueMissing) {
+      feedbackElement.textContent = 'This field is required.';
+    } else if (input.validity.typeMismatch) {
+      feedbackElement.textContent = 'Please enter a valid email.';
+    } else if (input.validity.tooShort) {
+      feedbackElement.textContent = `Please lengthen this email to ${input.minLength} characters or more. You are currently using ${input.value.length} characters.`;
     }
+    feedbackElement.style.display = 'block';
+  }
+}
+
+// Use the blur event to report validity once the user clicks off the element
+emailInput.addEventListener('blur', () => {
+  const feedbackElement = document.getElementById('emailFeedback');
+  if (!emailInput.validity.valid) {
+    emailFeedback(emailInput, feedbackElement);
+    feedbackElement.style.display = 'block';
+  }
 });
 
-// Remove feedback when input refocused
-// Try to make repeatable for all inputs!
-emailInput.addEventListener('click', function() {
+emailInput.addEventListener('input', () => {
+  const feedbackElement = document.getElementById('emailFeedback');
+  if (emailInput.validity.valid) {
     feedbackElement.style.display = 'none';
+  } else {
+    emailFeedback(emailInput, feedbackElement);
+  }
 });
 
