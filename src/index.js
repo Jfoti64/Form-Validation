3b366b579c323978ccaf3ecb511f0ec9b0bc6deb
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-09 22:44:42 -0500

    Add pattern validation to email input.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+import "./style.css";
+
+const emailInput = document.getElementById('email');
+const feedbackElement = document.getElementById('emailFeedback');
+
+emailInput.addEventListener('blur', function() {
+    if (this.validity.valid) {
+        // If the email input matches the pattern and is not empty
+        feedbackElement.style.display = 'none';
+    } else {
+        // If the input is invalid
+        feedbackElement.style.display = 'block';
+    }
+});
+
+// Remove feedback when input refocused
+// Try to make repeatable for all inputs!
+emailInput.addEventListener('click', function() {
+    feedbackElement.style.display = 'none';
+});
 
